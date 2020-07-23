3aa43950c326e3b890f2dc3878259aa6055c4121
Peter Kupchick <pkupchick@gmail.com>
2020-07-23 09:46:07 -0400

    Fixed up reducers and actions

--- frontend/bench.jsx
+++ frontend/bench.jsx
@@ -2,17 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { login, logout, signUp } from './util/session_api_util';
 import configureStore from './store/store';
+import { signin, makeNewUser } from './actions/session_actions';
 
 document.addEventListener("DOMContentLoaded", () => {
 
     const store = configureStore();
-    window.store = store;
+    // window.store = store;
     window.getState = store.getState;
     window.dispatch = store.dispatch;
     window.$ = $;
     window.login = login;
     window.logout = logout;
     window.signUp = signUp;
+    window.signin = signin;
+    window.makeNewUser = makeNewUser;
 
     const root = document.getElementById("root");
     ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
